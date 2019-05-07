9bb9fcd9e81808daea3f289b2f8ea48d2dda4843
Jan <crianonim@gmail.com>
2019-05-07 15:19:43 +0100

    basic

--- plain/index.js
+++ plain/index.js
@@ -1,34 +1,174 @@
 import Service from './service.js';
 
-Service.getStopID(53510).then(
-    (res)=>{
-        let id=res.id
-        console.log(res);
-        Service.getStopInfo(id).then(console.log)
-        Service.getArrivailAtStopID(id).then(
-            (arr)=>{
-                let processed=Service.sortByArrivalTime( 
-                    Service.extractArrivalInfo(arr)
-                )
-            
-                // processed.forEach(el=>console.log(JSON.stringify(el,null,2)))
-                processed.forEach(el=>{
-                    console.log(`${el.expectedArrival} ${el.lineName} ${el.destinationName}`);
-                });
-                renderResults(res,arr)
-            })
+let code = "";
+function displayStopBySmsCode(code) {
+    Service.getStopID(code).then(
+        (res) => {
+            let id = res.id
+            console.log(res);
+            Service.getStopInfo(id).then(console.log)
+            showArrivalsAtStop(res);
+        }
+
+    )
+}
+function displayStop(code) {
+
+    Service.getStopInfo(code).then(
+        (res) => {
+            let id = res.id
+            console.log(res);
+            Service.getStopInfo(id).then(console.log)
+            showArrivalsAtStop(res);
+        }
+
+    )
+}
+function updateCode() {
+    for (let i = 1; i < 6; i++) {
+        let el = document.getElementById('code' + i);
+        let digit = code[i - 1];
+        if (typeof digit == "undefined") digit = '';
+        el.innerText = digit;
     }
-)
-function renderResults(stopInfo,arrivals){
-    let s="<div class='results'>";
-    s+="<div class='stop-info'>";
-    s+=`<div><span class='letter'>${stopInfo.stopLetter}</span>
-        <span class='stop-name'>${stopInfo.name}</span> towards ${stopInfo.towards} 
+}
+function displaySMScodeEntry() {
+    document.getElementById('digits').addEventListener('click', (ev) => {
+        if (ev.target.classList.contains('digit')) {
+            let digit = ev.target.textContent;
+            if (digit == "X") {
+                code = "";
+            } else
+                if (digit == "<") {
+                    if (code.length > 0) {
+                        code = code.substr(0, code.length - 1)
+                    }
+                } else {
+                    if (code.length<5){
+                        code += digit;
+                        if (code.length==5){
+                            console.log("HAVE")
+                           displayStopBySmsCode(code)
+                        }
+                    }
+                   
+                }
+            console.log(code)
+            updateCode();
+        }
+    })
+    document.getElementById("smsCodeToggle").addEventListener('click',()=>{
+        console.log("Toggle")
+        document.getElementById('code').classList.toggle('hidden');
+        document.getElementById('digits').classList.toggle('hidden');
+
+    })
+}
+displaySMScodeEntry()
+// displayStop('490000055A');
+//displayStopBySmsCode(72538);
+let re = Service.getStopsWithinRadius(500);
+re.then((stops) => {
+    console.log("Have stops", stops);
+    let s = `<div class='around'><div class='around-header'>Nearby Bus stops</div>`;
+    stops.forEach(stop => {
+        if (!stop.lines.length) return ;    
+        s += `<div class='stop' data-stop-id='${stop.id}'>`
+        s += `<span class='letter'>${stop.stopLetter.replace('->', '')}</span>`;
+        s += `<div class='stop-main-info'><span><span class='stop-name'>${stop.name} </span>`;
+        s += ` towards ${stop.towards}</span>`
+        s += `<span>${stop.lines.join(',')}</span></div>`
+        s += `</div>`; //stop
+    })
+    s += `</div>`; //around
+    let around = document.getElementById('around')
+    around.innerHTML = s;
+    around.querySelectorAll('.stop').forEach(stop => {
+        stop.addEventListener('click', (ev) => {
+            displayStop(ev.currentTarget.dataset.stopId)
+            console.log("Clicked on id", ev.currentTarget.dataset.stopId)
+        })
+    })
+})
+function showArrivalsAtStop(stopInfo) {
+    let id = stopInfo.id;
+    Service.getArrivailAtStopID(id).then(
+        (arr) => {
+            let processed = Service.sortByArrivalTime(arr)
+            console.log(arr)
+            // processed.forEach(el=>console.log(JSON.stringify(el,null,2)))
+            processed.forEach(el => {
+                console.log(`${el.expectedArrival} ${el.lineName} ${el.destinationName}`);
+            });
+            stopInfo.timestamp = Date.now();
+            stopInfo.linesExcluded = stopInfo.linesExcluded || [];
+            renderResults(stopInfo, processed)
+            setInterval(updateTimes, 1000);
+        })
+}
+function updateTimes() {
+    document.querySelectorAll('.time-to-station').forEach(el => {
+        let time = el.dataset.arrivalTime;
+        let difference = Service.timeDifference(new Date(), time);
+        el.textContent = Service.secondsToTime(difference);
+        // console.log(time);
+    })
+    let ago = document.getElementById('updated-ago');
+    ago.textContent = Service.secondsToTime(((Date.now() - ago.dataset.updatedAgo) / 1000) >> 0);
+}
+function renderResults(stopInfo, arrivals) {
+
+    let s = "<div class='results'>";
+    s += "<div class='stop-info'>";
+    s += `<div class='stop-title-line'><span class='letter'>${stopInfo.stopLetter.replace('->', '')}</span>
+       <div class='stop-title-text'> 
+        <span class='stop-name'>${stopInfo.name}</span>
+        <span class='stop-towards'> towards ${stopInfo.towards} </span>
+        </div> 
         </div>`
+    s += "<div class='lines'>";
+    stopInfo.lines.forEach(el => {
+        let isExcluded = stopInfo.linesExcluded.includes(el) ? "excluded" : "";
+        s += `<span class='line ${isExcluded}'>${el}</span>`;
+    })
+    s += "</div>";//lines
+    s += "<div class='update-info'>"
+    s += "<span class='update-info-content'>"
+    s += `<span>Updated at ${Service.extractTimeFromISODateString(stopInfo.timestamp)}</span> `
+    s += `<span id='updated-ago' data-updated-ago='${stopInfo.timestamp}'>${Service.secondsToTime(((Date.now() - stopInfo.timestamp) / 1000) >> 0)}</span> ago. `
+    s += `</span>`
+    s += "<span class='button' id='update'>Update</span> "
+    s += "</div>";
 
-    s+="</div>"; // stop-info
+    s += "</div>"; // stop-info
 
-    s+=
-    s+="</div>"; // results
-    document.getElementById('app').innerHTML=s;
+    s += "<div class='arrivals'>"
+
+    arrivals.filter(arr => !stopInfo.linesExcluded.includes(arr.lineName))
+        .forEach(arr => {
+            s += `<div class='arrival'>`;
+            s += `<span class='arrival-time' >${Service.extractTimeFromISODateString(arr.expectedArrival)}</span>`;
+            s += `<span class='time-to-station' data-arrival-time='${arr.expectedArrival}'>${Service.secondsToTime(Service.timeDifference(new Date(), arr.expectedArrival))}</span>`;
+            s += `<span class='line-name'>${arr.lineName}</span>`;
+            s += `<span class='destination'>${arr.destinationName}</span>`
+            s += `</div>`
+        })
+    s += "</div>";//arrivals
+    s += "</div>"; // results
+    document.getElementById('arrivals').innerHTML = s;
+    document.querySelector('.lines').addEventListener("click", (ev) => {
+        if (ev.target.classList.contains("line")) {
+            let lineName = ev.target.textContent;
+            if (stopInfo.linesExcluded.includes(lineName)) {
+                stopInfo.linesExcluded = stopInfo.linesExcluded.filter(el => el != lineName);
+            } else {
+                stopInfo.linesExcluded.push(lineName);
+            }
+            console.log(stopInfo.linesExcluded)
+            renderResults(stopInfo, arrivals)
+        }
+    });
+    document.getElementById('update').addEventListener('click', () => {
+        showArrivalsAtStop(stopInfo);
+    })
 }
--- plain/service.js
+++ plain/service.js
@@ -13,27 +13,71 @@ function extractArrivalInfo(arr){
 function sortByArrivalTime(arr){
     return arr.sort( (a,b)=> a.timeToStation-b.timeToStation );
 }
-export default {
-    async getStopID(smsCode) {
-        let response=await fetch(baseUrl + '/StopPoint/Search?query=' + smsCode);
+function secondsToTime(s){
+    if (s<0) return "due";
+    let mins=(s/60)>>0;
+    let secs=s-mins*60;
+    
+    return (mins+"").padStart(2,"0")+":"+(secs+"").padStart(2,"0")
+}
+function timeDifference(start,end){
+  
+    return ((new Date(end)-new Date(start))/1000)>>0
+}
+async function getStopsWithinRadius(r=200){
+
+  return new Promise( (resolve)=>{
+    navigator.geolocation.getCurrentPosition( async (pos)=>{
+        let {coords}=pos;
+        console.log(coords);
+        let lat=coords.latitude;
+        let lon=coords.longitude;
+        let response=await fetch(baseUrl+'/StopPoint?stopTypes=NaptanPublicBusCoachTram&radius='+r+'&lat='+lat+'&lon='+lon);
         let json=await response.json();
-        if (json.total==0){
-            console.log("No stops found");
-            return null;
-        } else {
-            let {id,towards,name,stopLetter}=json.matches[0];
-            return {id,towards,name,stopLetter}
-        }
-    },
+        console.log(json.stopPoints.length)
+        let stops=json.stopPoints.map(stop=>{
+            let lines=stop.lines.map(el=>el.name)
+            let towards=stop.additionalProperties.find(el=>el.key=="Towards");
+            if (towards) towards=towards.value;
+            let stopInfo={stopLetter:stop.stopLetter,lines,id:stop.id,name:stop.commonName,distance:stop.distance,towards};
+            return stopInfo;
+        })
+        // console.log(stops)
+        // console.log(JSON.stringify(json,null,2));
+        resolve(stops)
+    })
+  })  
+  
+}
+async function getStopID(smsCode) {
+    let response=await fetch(baseUrl + '/StopPoint/Search?query=' + smsCode);
+    let json=await response.json();
+    if (json.total==0){
+        console.log("No stops found");
+        return null;
+    } else {
+        console.log(json)
+        let {id,towards,name,stopLetter,lines}=json.matches[0];
+        lines=lines.map(el=>el.name);
+        return {id,towards,name,stopLetter,lines}
+    }
+}
+export default {
+    getStopID,
     async getArrivailAtStopID(id){
         let response=await fetch(baseUrl + '/StopPoint/'+id+'/Arrivals');
         return await response.json();
     },
+    //not working
     async getStopInfo(id){
         let response=await fetch(baseUrl + '/StopPoint/'+id);
-        return await response.json();
+        let json=await response.json();
+        return getStopID(json.smsCode) 
     },
     extractTimeFromISODateString,
     extractArrivalInfo,
     sortByArrivalTime,
+    secondsToTime,
+    timeDifference,
+    getStopsWithinRadius,
 }
