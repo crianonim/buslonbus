44fe6d3ce9f2546ef48717fbfc54d862e758b382
Jan Skowronski <crianonim@gmail.com>
2019-08-17 21:30:18 +0100

    Tidy up and functional parity

--- plain/index.js
+++ plain/index.js
@@ -5,7 +5,6 @@ window.addEventListener("load", () => {
     getNearby();
 });
 
-
 const fakeLog = s => {
     document.getElementById("console").textContent += `${JSON.stringify(s)}
   `;
@@ -17,7 +16,7 @@ if (location.search === "?debug") {
 }
 let code = "";
 
-function displayStopBySmsCode(code) {
+const displayStopBySmsCode = (code) => {
     Service.getStopID(code).then(res => {
         const id = res.id;
         Service.getStopInfo(id).then(console.log);
@@ -30,7 +29,7 @@ function displayStopBySmsCode(code) {
     });
 }
 
-function displayStop(code) {
+const displayStop = (code) => {
     Service.getStopInfo(code).then(res => {
         const id = res.id;
         Service.getStopInfo(id).then(console.log);
@@ -38,7 +37,7 @@ function displayStop(code) {
     });
 }
 
-function updateCode() {
+const updateCode = () => {
     for (let i = 1; i < 6; i++) {
         const el = document.getElementById("code" + i);
         let digit = code[i - 1];
@@ -47,7 +46,7 @@ function updateCode() {
     }
 }
 
-function displaySMScodeEntry() {
+const displaySMScodeEntry = () => {
     document.getElementById("digits").addEventListener("click", ev => {
         if (ev.target.classList.contains("digit")) {
             const digit = ev.target.textContent;
@@ -61,7 +60,6 @@ function displaySMScodeEntry() {
                 if (code.length < 5) {
                     code += digit;
                     if (code.length === 5) {
-                        // console.log("HAVE")
                         displayStopBySmsCode(code);
                     }
                 }
@@ -89,42 +87,17 @@ const renderStopComponent = (stop) => {
     return el;
 }
 
-// function renderStop(stop) {
-//   return !stop.lines.length
-//     ? ""
-//     : `
-//     <div class='stop' data-stop-id='${stop.id}'>
-//      <span class='letter'>${(stop.stopLetter || "-").replace("->", "")}</span>
-//      <div class='stop-main-info'>
-//       <span>
-//        <span class='stop-name'>${stop.name}</span>
-//        ${stop.towards?` towards ${stop.towards}`:`` } 
-//       </span>
-//       <span>${stop.lines.join(", ")}</span>
-//     </div>
-//     </div>
-//        `;
-// }
-function getNearby() {
+const getNearby = () => {
     Service.getStopsWithinRadius(500)
         .then(stops => {
             const aroundOrig = document.querySelector(".around");
             const around = aroundOrig.cloneNode(true);
-            console.log(around);
             stops.filter(stop => stop.lines.length)
                 .map(renderStopComponent)
                 .forEach(around.appendChild.bind(around));
-
-            // around.innerHTML = `
-            //   <div class='around'>
-            //    <div class='around-header'>Nearby Bus stops</div>
-            //    ${stops.map(renderStop).join("")}
-            //   </div>
-            //  `;
             around.querySelectorAll(".stop").forEach(stop => {
                 stop.addEventListener("click", ev => {
                     displayStop(ev.currentTarget.dataset.stopId);
-                    // console.log("Clicked on id", ev.currentTarget.dataset.stopId)
                 });
             });
             window.requestAnimationFrame(() => {
@@ -136,13 +109,10 @@ function getNearby() {
         });
 }
 
-function showArrivalsAtStop(stopInfo) {
+const showArrivalsAtStop = (stopInfo) => {
     const id = stopInfo.id;
     Service.getArrivailAtStopID(id).then(arr => {
         const processed = Service.sortByArrivalTime(arr);
-        processed.forEach(el => {
-            // console.log(`${el.expectedArrival} ${el.lineName} ${el.destinationName}`);
-        });
         stopInfo.timestamp = Date.now();
         stopInfo.linesExcluded = stopInfo.linesExcluded || [];
         renderResultsComponent(stopInfo, processed);
@@ -150,24 +120,16 @@ function showArrivalsAtStop(stopInfo) {
     });
 }
 
-function updateTimes() {
+const updateTimes = () => {
     document.querySelectorAll(".time-to-station").forEach(el => {
         const time = el.dataset.arrivalTime;
         const difference = Service.timeDifference(new Date(), time);
         el.textContent = Service.secondsToTime(difference);
-        // console.log(time);
     });
     const ago = document.querySelector(".updated-ago");
-    ago.textContent = Service.secondsToTime(
-        ((Date.now() - ago.dataset.updatedAgo) / 1000) >> 0
-    );
+    ago.textContent = Service.secondsToTime(((Date.now() - ago.dataset.updatedAgo) / 1000) >> 0);
 }
-// WIP
-function renderLine(el) {
-    return `
-    <span class='line'>${el}</span>
-    `
-}
+
 const stopLetterCorrected = (stopLetter) => (stopLetter || "-").replace("->", "");
 
 const renderResultsComponent = (stopInfo, arrivals) => {
@@ -209,70 +171,7 @@ const renderResultsComponent = (stopInfo, arrivals) => {
         })
         .forEach(arrivalElement => arrivalsDiv.appendChild(arrivalElement));
 
-    el.querySelector('.update').addEventListener("click", () => {
-        showArrivalsAtStop(stopInfo);
-    })
-    arrivalsNew.appendChild(el);
-    window.requestAnimationFrame(() => {
-        arrivalsOrig.replaceWith(arrivalsNew);
-    })
-}
-
-function renderResults(stopInfo, arrivals) {
-    let s = `<div class='results'>
-  <div class='stop-info'>
-  <div class='stop-title-line'>
-  <span class='letter'>${stopInfo.stopLetter.replace("->", "")}</span>
-       <div class='stop-title-text'> 
-        <span class='stop-name'>${stopInfo.name}</span>
-        <span class='stop-towards'> towards ${stopInfo.towards} </span>
-        </div>
-        <div class='make-favourite'> + </div>
-        </div>
-   <div class='lines'>`;
-    stopInfo.lines.forEach(el => {
-        let isExcluded = stopInfo.linesExcluded.includes(el) ? "excluded" : "";
-        s += `<span class='line ${isExcluded}'>${el}</span>`;
-    });
-    s += "</div>"; //lines
-    s += "<div class='update-info'>";
-    s += "<span class='update-info-content'>";
-    s += `<span>Updated at ${Service.extractTimeFromISODateString(
-    stopInfo.timestamp
-  )}</span> `;
-    s += `<span id='updated-ago' data-updated-ago='${
-    stopInfo.timestamp
-  }'>${Service.secondsToTime(
-    ((Date.now() - stopInfo.timestamp) / 1000) >> 0
-  )}</span> ago. `;
-    s += `</span>`;
-    s += "<span class='button' id='update'>Update</span> ";
-    s += "</div>";
-
-    s += "</div>"; // stop-info
-
-    s += "<div class='arrivals'>";
-
-    arrivals
-        .filter(arr => !stopInfo.linesExcluded.includes(arr.lineName))
-        .forEach(arr => {
-            s += `<div class='arrival'>`;
-            s += `<span class='arrival-time' >${Service.extractTimeFromISODateString(
-        arr.expectedArrival
-      )}</span>`;
-            s += `<span class='time-to-station' data-arrival-time='${
-        arr.expectedArrival
-      }'>${Service.secondsToTime(
-        Service.timeDifference(new Date(), arr.expectedArrival)
-      )}</span>`;
-            s += `<span class='line-name'>${arr.lineName}</span>`;
-            s += `<span class='destination'>${arr.destinationName}</span>`;
-            s += `</div>`;
-        });
-    s += "</div>"; //arrivals
-    s += "</div>"; // results
-    document.getElementById("arrivals").innerHTML = s;
-    document.querySelector(".lines").addEventListener("click", ev => {
+    el.querySelector(".lines").addEventListener("click", ev => {
         if (ev.target.classList.contains("line")) {
             let lineName = ev.target.textContent;
             if (stopInfo.linesExcluded.includes(lineName)) {
@@ -282,11 +181,15 @@ function renderResults(stopInfo, arrivals) {
             } else {
                 stopInfo.linesExcluded.push(lineName);
             }
-            // console.log(stopInfo.linesExcluded)
-            renderResults(stopInfo, arrivals);
+            renderResultsComponent(stopInfo, arrivals);
         }
     });
-    document.getElementById("update").addEventListener("click", () => {
+
+    el.querySelector('.update').addEventListener("click", () => {
         showArrivalsAtStop(stopInfo);
-    });
+    })
+    arrivalsNew.appendChild(el);
+    window.requestAnimationFrame(() => {
+        arrivalsOrig.replaceWith(arrivalsNew);
+    })
 }
