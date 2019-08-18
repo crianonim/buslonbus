8e39f290c467e91a9e30ebb9d422819d0e87c8f6
Jan Skowronski <crianonim@gmail.com>
2019-08-18 09:04:38 +0100

    Tidy up

--- elements.js
+++ elements.js
@@ -1,55 +1,6 @@
 import $ from './dom.js';
 
-class PopUpInfo extends HTMLElement {
-    constructor() {
-        // Always call super first in constructor
-        super();
 
-        // write element functionality in here
-
-        // Create a shadow root
-        var shadow = this.attachShadow({
-            mode: 'open'
-        });
-
-        // Create spans
-        var wrapper = document.createElement('span');
-        wrapper.setAttribute('class', 'wrapper');
-        var icon = document.createElement('span');
-        icon.setAttribute('class', 'icon');
-        icon.setAttribute('tabindex', 0);
-        var info = document.createElement('span');
-        info.setAttribute('class', 'info');
-
-        // Take attribute content and put it inside the info span
-        var text = this.getAttribute('text');
-        info.textContent = text;
-
-        // Insert icon
-        var imgUrl;
-        if (this.hasAttribute('img')) {
-            imgUrl = this.getAttribute('img');
-        } else {
-            imgUrl = 'img/default.png';
-        }
-        var img = document.createElement('img');
-        img.src = imgUrl;
-        icon.appendChild(img);
-
-        // Create some CSS to apply to the shadow dom
-        var style = document.createElement('style');
-
-        style.textContent = '.wrapper {' +
-            // CSS truncated for brevity
-
-            // attach the created elements to the shadow dom
-
-            shadow.appendChild(style);
-        shadow.appendChild(wrapper);
-        wrapper.appendChild(icon);
-        wrapper.appendChild(info);
-    }
-}
 class BusLine extends HTMLElement {
     connectedCallback() {
         const shadow = this.attachShadow({
@@ -65,18 +16,7 @@ class BusLine extends HTMLElement {
         }
         shadow.appendChild(span);
     }
-} {
-    /* <div class="stop" data-stop-id="">
-            <span class="letter"></span>
-            <div class="stop-main-info">
-              <span>
-                <span class="stop-name"></span>
-                <span class="stop-towards"></span>
-              </span>
-              <span class="stop-lines"></span>
-            </div>
-          </div> */
-}
+} 
 class BusStop extends HTMLElement {
 
     connectedCallback() {
@@ -106,5 +46,4 @@ class BusStop extends HTMLElement {
     }
 }
 customElements.define('bus-stop', BusStop)
-customElements.define('popup-info', PopUpInfo);
 customElements.define('bus-line', BusLine);
