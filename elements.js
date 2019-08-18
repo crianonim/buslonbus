2a9649c7782f8a5a726ce6df6d66286878b45cb7
Jan Skowronski <crianonim@gmail.com>
2019-08-18 07:41:08 +0100

    Create BUS-LINE custom element and use in the app

---
+++ elements.js
@@ -0,0 +1,68 @@
+class PopUpInfo extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+
+        // write element functionality in here
+
+        // Create a shadow root
+        var shadow = this.attachShadow({
+            mode: 'open'
+        });
+
+        // Create spans
+        var wrapper = document.createElement('span');
+        wrapper.setAttribute('class', 'wrapper');
+        var icon = document.createElement('span');
+        icon.setAttribute('class', 'icon');
+        icon.setAttribute('tabindex', 0);
+        var info = document.createElement('span');
+        info.setAttribute('class', 'info');
+
+        // Take attribute content and put it inside the info span
+        var text = this.getAttribute('text');
+        info.textContent = text;
+
+        // Insert icon
+        var imgUrl;
+        if (this.hasAttribute('img')) {
+            imgUrl = this.getAttribute('img');
+        } else {
+            imgUrl = 'img/default.png';
+        }
+        var img = document.createElement('img');
+        img.src = imgUrl;
+        icon.appendChild(img);
+
+        // Create some CSS to apply to the shadow dom
+        var style = document.createElement('style');
+
+        style.textContent = '.wrapper {' +
+            // CSS truncated for brevity
+
+            // attach the created elements to the shadow dom
+
+            shadow.appendChild(style);
+        shadow.appendChild(wrapper);
+        wrapper.appendChild(icon);
+        wrapper.appendChild(info);
+    }
+}
+class BusLine extends HTMLElement {
+    connectedCallback() {
+        const shadow = this.attachShadow({
+            mode: 'open'
+        });
+        const span = document.createElement('span');
+        span.classList.add('line');
+        if (this.hasAttribute('line')){
+            span.textContent=this.getAttribute('line');
+        }
+        if (this.hasAttribute('excluded')){
+            this.classList.add('excluded')
+        }
+        shadow.appendChild(span);
+    }
+}
+customElements.define('popup-info', PopUpInfo);
+customElements.define('bus-line', BusLine);
