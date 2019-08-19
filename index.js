60113604bbe4a158c32a498879946be10ddebef9
Jan Skowronski <crianonim@gmail.com>
2019-08-19 09:34:40 +0100

    Setup click listeners on tabs and call selectTab

--- index.js
+++ index.js
@@ -1,207 +1,244 @@
 import Service from "./service.js";
-import './elements.js';
-import $ from './dom.js';
+import "./elements.js";
+import $ from "./dom.js";
 
 window.addEventListener("load", () => {
-    displaySMScodeEntry();
-    getNearby();
+  setupTabs();
+  displaySMScodeEntry();
+  getNearby();
 });
 
+
+const setupTabs = () => {
+  document.querySelectorAll(".tab").forEach(tab => {
+    tab.addEventListener("click", e => {
+      selectTab(
+        Array.from(e.target.parentElement.children).findIndex(el => el === tab)
+      );
+    });
+  });
+  selectTab(0)
+};
+const selectTab = tab => {
+    console.log(tab);
+  };
 const fakeLog = s => {
-    document.getElementById("console").textContent += `${JSON.stringify(s)}
+  document.getElementById("console").textContent += `${JSON.stringify(s)}
   `;
 };
 if (location.search === "?debug") {
-    console.log = fakeLog;
-    console.error = fakeLog;
-    console.warn = fakeLog;
+  console.log = fakeLog;
+  console.error = fakeLog;
+  console.warn = fakeLog;
 }
 let code = "";
 
-const displayStopBySmsCode = (code) => {
-    Service.getStopID(code).then(res => {
-        const id = res.id;
-        Service.getStopInfo(id).then(console.log);
-        showArrivalsAtStop(res);
-    }).catch(err => {
-        // TODO send info that bad code
-        console.log({
-            err
-        });
+const displayStopBySmsCode = code => {
+  Service.getStopID(code)
+    .then(res => {
+      const id = res.id;
+      Service.getStopInfo(id).then(console.log);
+      showArrivalsAtStop(res);
+    })
+    .catch(err => {
+      // TODO send info that bad code
+      console.log({
+        err
+      });
     });
-}
+};
 
-const displayStop = (code) => {
-    Service.getStopInfo(code).then(res => {
-        const id = res.id;
-        Service.getStopInfo(id).then(console.log);
-        showArrivalsAtStop(res);
-    });
-}
+const displayStop = code => {
+  Service.getStopInfo(code).then(res => {
+    const id = res.id;
+    Service.getStopInfo(id).then(console.log);
+    showArrivalsAtStop(res);
+  });
+};
 
 const updateCode = () => {
-    for (let i = 1; i < 6; i++) {
-        const el = document.getElementById("code" + i);
-        let digit = code[i - 1];
-        if (typeof digit == "undefined") digit = "";
-        el.innerText = digit;
-    }
-}
+  for (let i = 1; i < 6; i++) {
+    const el = document.getElementById("code" + i);
+    let digit = code[i - 1];
+    if (typeof digit == "undefined") digit = "";
+    el.innerText = digit;
+  }
+};
 
 const displaySMScodeEntry = () => {
-    document.getElementById("digits").addEventListener("click", ev => {
-        if (ev.target.classList.contains("digit")) {
-            const digit = ev.target.textContent;
-            if (digit === "X") {
-                code = "";
-            } else if (digit === "<") {
-                if (code.length > 0) {
-                    code = code.substr(0, code.length - 1);
-                }
-            } else {
-                if (code.length < 5) {
-                    code += digit;
-                    if (code.length === 5) {
-                        displayStopBySmsCode(code);
-                    }
-                }
-            }
-            updateCode();
+  document.getElementById("digits").addEventListener("click", ev => {
+    if (ev.target.classList.contains("digit")) {
+      const digit = ev.target.textContent;
+      if (digit === "X") {
+        code = "";
+      } else if (digit === "<") {
+        if (code.length > 0) {
+          code = code.substr(0, code.length - 1);
         }
-    });
-    document.getElementById("smsCodeToggle").addEventListener("click", () => {
-        document.getElementById("code").classList.toggle("hidden");
-        document.getElementById("digits").classList.toggle("hidden");
-    });
-}
-
-const getTemplate = (id) => document.getElementById(id).cloneNode(true).content.firstElementChild;
-
-const renderStopComponent = (stop) => $('bus-stop', {
-    dataset: {
-        stop: JSON.stringify(stop)
+      } else {
+        if (code.length < 5) {
+          code += digit;
+          if (code.length === 5) {
+            displayStopBySmsCode(code);
+          }
+        }
+      }
+      updateCode();
     }
-})
+  });
+  document.getElementById("smsCodeToggle").addEventListener("click", () => {
+    document.getElementById("code").classList.toggle("hidden");
+    document.getElementById("digits").classList.toggle("hidden");
+  });
+};
 
-const replaceElement = (orignal, cloneDeep=true, cb) => {
-    const el = orignal.cloneNode(cloneDeep);
-    window.requestAnimationFrame(() => {
-        orignal.replaceWith(el);
-        if (cb) cb();
-    })
-    return el;
-}
+const getTemplate = id =>
+  document.getElementById(id).cloneNode(true).content.firstElementChild;
+
+const renderStopComponent = stop =>
+  $("bus-stop", {
+    dataset: {
+      stop: JSON.stringify(stop)
+    }
+  });
+
+const replaceElement = (orignal, cloneDeep = true, cb) => {
+  const el = orignal.cloneNode(cloneDeep);
+  window.requestAnimationFrame(() => {
+    orignal.replaceWith(el);
+    if (cb) cb();
+  });
+  return el;
+};
 
 const getNearby = () => {
-    Service.getStopsWithinRadius(500)
-        .then(stops => {
-            const around = replaceElement(document.querySelector(".around"), true,() => {
-                around.querySelectorAll(".stop").forEach(stop => {
-                    stop.addEventListener("click", ev => {
-                        displayStop(ev.currentTarget.dataset.stopId);
-                    });
-                });
-            })
-            stops.filter(stop => stop.lines.length)
-                .map(renderStopComponent)
-                .forEach(around.appendChild.bind(around));
-        })
-        .catch(reason => {
-            console.log(reason);
-        });
-}
-
-const showArrivalsAtStop = (stopInfo) => {
-    const id = stopInfo.id;
-    Service.getArrivailAtStopID(id).then(arr => {
-        const processed = Service.sortByArrivalTime(arr);
-        stopInfo.timestamp = Date.now();
-        stopInfo.linesExcluded = stopInfo.linesExcluded || [];
-        renderResultsComponent(stopInfo, processed);
-        setInterval(updateTimes, 1000);
+  Service.getStopsWithinRadius(500)
+    .then(stops => {
+      const around = replaceElement(
+        document.querySelector(".around"),
+        true,
+        () => {
+          around.querySelectorAll(".stop").forEach(stop => {
+            stop.addEventListener("click", ev => {
+              displayStop(ev.currentTarget.dataset.stopId);
+            });
+          });
+        }
+      );
+      stops
+        .filter(stop => stop.lines.length)
+        .map(renderStopComponent)
+        .forEach(around.appendChild.bind(around));
+    })
+    .catch(reason => {
+      console.log(reason);
     });
-}
+};
+
+const showArrivalsAtStop = stopInfo => {
+  const id = stopInfo.id;
+  Service.getArrivailAtStopID(id).then(arr => {
+    const processed = Service.sortByArrivalTime(arr);
+    stopInfo.timestamp = Date.now();
+    stopInfo.linesExcluded = stopInfo.linesExcluded || [];
+    renderResultsComponent(stopInfo, processed);
+    setInterval(updateTimes, 1000);
+  });
+};
 
 const updateTimes = () => {
-    document.querySelectorAll(".time-to-station").forEach(el => {
-        const time = el.parentElement.parentElement.dataset.arrivalTime;
-        const difference = Service.timeDifference(new Date(), time);
-        el.textContent = Service.secondsToTime(difference);
-    });
-    const ago = document.querySelector(".updated-ago");
-    ago.textContent = Service.secondsToTime(((Date.now() - ago.dataset.updatedAgo) / 1000) >> 0);
-}
+  document.querySelectorAll(".time-to-station").forEach(el => {
+    const time = el.parentElement.parentElement.dataset.arrivalTime;
+    const difference = Service.timeDifference(new Date(), time);
+    el.textContent = Service.secondsToTime(difference);
+  });
+  const ago = document.querySelector(".updated-ago");
+  ago.textContent = Service.secondsToTime(
+    ((Date.now() - ago.dataset.updatedAgo) / 1000) >> 0
+  );
+};
 
-const stopLetterCorrected = (stopLetter) => (stopLetter || "-").replace("->", "");
+const stopLetterCorrected = stopLetter => (stopLetter || "-").replace("->", "");
 
 const renderResultsComponent = (stopInfo, arrivals) => {
-    const arrivalsOrig = document.getElementById('arrivals');
-    const arrivalsNew = arrivalsOrig.cloneNode(false);
-    const el = getTemplate('template-results');
-    el.querySelector('.letter').textContent = stopLetterCorrected(stopInfo.stopLetter);
-    el.querySelector('.stop-name').textContent = stopInfo.name;
-    if (stopInfo.towards) {
-        el.querySelector('.stop-towards').textContent = "towards " + stopInfo.towards
+  const arrivalsOrig = document.getElementById("arrivals");
+  const arrivalsNew = arrivalsOrig.cloneNode(false);
+  const el = getTemplate("template-results");
+  el.querySelector(".letter").textContent = stopLetterCorrected(
+    stopInfo.stopLetter
+  );
+  el.querySelector(".stop-name").textContent = stopInfo.name;
+  if (stopInfo.towards) {
+    el.querySelector(".stop-towards").textContent =
+      "towards " + stopInfo.towards;
+  }
+  // const lineTemplate = getTemplate('template-line');
+  const linesDiv = el.querySelector(".lines");
+  stopInfo.lines.forEach(line => {
+    const lineElement = document.createElement("bus-line");
+    if (stopInfo.linesExcluded.includes(line)) {
+      lineElement.setAttribute("excluded", true);
     }
-    // const lineTemplate = getTemplate('template-line');
-    const linesDiv = el.querySelector('.lines');
-    stopInfo.lines.forEach(line => {
-        const lineElement = document.createElement('bus-line');
-        if (stopInfo.linesExcluded.includes(line)) {
-            lineElement.setAttribute('excluded', true);
-        }
-        lineElement.setAttribute('line', line);
-        linesDiv.appendChild(lineElement);
+    lineElement.setAttribute("line", line);
+    linesDiv.appendChild(lineElement);
+  });
+  el.querySelector(
+    ".updated-at"
+  ).textContent = Service.extractTimeFromISODateString(stopInfo.timestamp);
+  const updatedAgo = el.querySelector(".updated-ago");
+  updatedAgo.dataset.updatedAgo = stopInfo.timestamp;
+  updatedAgo.textContent = Service.secondsToTime(
+    ((Date.now() - stopInfo.timestamp) / 1000) >> 0
+  );
+
+  const arrivalsDiv = el.querySelector(".arrivals");
+  arrivals
+    .filter(arr => !stopInfo.linesExcluded.includes(arr.lineName))
+    .map(arrival => {
+      //to be moved to Service
+      arrival.timeToStation = Service.secondsToTime(
+        Service.timeDifference(new Date(), arrival.expectedArrival)
+      );
+      arrival.arrivalTime = Service.extractTimeFromISODateString(
+        arrival.expectedArrival
+      );
+      const arrivalElement = document.createElement("bus-arrival");
+      arrivalElement.dataset.arrivalTime = arrival.expectedArrival;
+      arrivalElement.setAttribute("arrival", JSON.stringify(arrival));
+      // const arrivalElement = getTemplate('template-arrival');
+      // arrivalElement.querySelector('.arrival-time').textContent = Service.extractTimeFromISODateString(arrival.expectedArrival);
+      // const timeToStation = arrivalElement.querySelector('.time-to-station');
+      // timeToStation.dataset.arrivalTime = arrival.expectedArrival;
+      // timeToStation.textContent = Service.secondsToTime(Service.timeDifference(new Date(), arrival.expectedArrival));
+      // arrivalElement.querySelector('.line-name').textContent = arrival.lineName;
+      // arrivalElement.querySelector('.destination').textContent = arrival.destinationName;
+      return arrivalElement;
     })
-    el.querySelector('.updated-at').textContent = Service.extractTimeFromISODateString(stopInfo.timestamp)
-    const updatedAgo = el.querySelector('.updated-ago');
-    updatedAgo.dataset.updatedAgo = stopInfo.timestamp;
-    updatedAgo.textContent = Service.secondsToTime(((Date.now() - stopInfo.timestamp) / 1000) >> 0)
+    .forEach(arrivalElement => arrivalsDiv.appendChild(arrivalElement));
 
-    const arrivalsDiv = el.querySelector('.arrivals');
-    arrivals
-        .filter(arr => !stopInfo.linesExcluded.includes(arr.lineName))
-        .map(arrival => {
-            //to be moved to Service
-            arrival.timeToStation= Service.secondsToTime(Service.timeDifference(new Date(), arrival.expectedArrival));
-            arrival.arrivalTime=Service.extractTimeFromISODateString(arrival.expectedArrival);
-            const arrivalElement=document.createElement('bus-arrival');
-            arrivalElement.dataset.arrivalTime = arrival.expectedArrival;
-            arrivalElement.setAttribute('arrival',JSON.stringify(arrival));
-            // const arrivalElement = getTemplate('template-arrival');
-            // arrivalElement.querySelector('.arrival-time').textContent = Service.extractTimeFromISODateString(arrival.expectedArrival);
-            // const timeToStation = arrivalElement.querySelector('.time-to-station');
-            // timeToStation.dataset.arrivalTime = arrival.expectedArrival;
-            // timeToStation.textContent = Service.secondsToTime(Service.timeDifference(new Date(), arrival.expectedArrival));
-            // arrivalElement.querySelector('.line-name').textContent = arrival.lineName;
-            // arrivalElement.querySelector('.destination').textContent = arrival.destinationName;
-            return arrivalElement;
-        })
-        .forEach(arrivalElement => arrivalsDiv.appendChild(arrivalElement));
+  el.querySelector(".lines").addEventListener("click", ev => {
+    if (ev.target.tagName === "BUS-LINE") {
+      let lineName = ev.target.getAttribute("line");
+      if (stopInfo.linesExcluded.includes(lineName)) {
+        stopInfo.linesExcluded = stopInfo.linesExcluded.filter(
+          el => el != lineName
+        );
+      } else {
+        stopInfo.linesExcluded.push(lineName);
+      }
+      renderResultsComponent(stopInfo, arrivals);
+    }
+  });
 
-    el.querySelector(".lines").addEventListener("click", ev => {
-        if (ev.target.tagName === "BUS-LINE") {
-            let lineName = ev.target.getAttribute('line');
-            if (stopInfo.linesExcluded.includes(lineName)) {
-                stopInfo.linesExcluded = stopInfo.linesExcluded.filter(
-                    el => el != lineName
-                );
-            } else {
-                stopInfo.linesExcluded.push(lineName);
-            }
-            renderResultsComponent(stopInfo, arrivals);
-        }
-    });
+  el.querySelector(".update").addEventListener("click", () => {
+    showArrivalsAtStop(stopInfo);
+  });
+  arrivalsNew.appendChild(el);
+  window.requestAnimationFrame(() => {
+    arrivalsOrig.replaceWith(arrivalsNew);
+  });
+};
 
-    el.querySelector('.update').addEventListener("click", () => {
-        showArrivalsAtStop(stopInfo);
-    })
-    arrivalsNew.appendChild(el);
-    window.requestAnimationFrame(() => {
-        arrivalsOrig.replaceWith(arrivalsNew);
-    })
-}
-
-const ba=document.createElement('bus-arrival');
-ba.setAttribute('bus','n55');
+const ba = document.createElement("bus-arrival");
+ba.setAttribute("bus", "n55");
 document.body.appendChild(ba);
