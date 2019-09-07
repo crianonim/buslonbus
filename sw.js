bcc8ed43a564e10be9fea3790f6d779b87fc4f0f
Jan Skowronski <crianonim@gmail.com>
2019-09-07 10:57:11 +0100

    Add manifesr and icons

---
+++ sw.js
@@ -0,0 +1,42 @@
+const cacheName = "bus";
+const files = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/elements.js",
+  "/style.css",
+  "/storage.js",
+  "/service.js",
+  "/dom.js",
+  "/img/cancel.svg",
+  "/img/gps.svg",
+  "/img/dial.svg",
+  "/img/star.svg",
+  "/img/update.svg"
+
+];
+
+self.addEventListener("install", e => {
+  console.log("[ServiceWorker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(files);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+    console.log("Activate")
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    console.log("FETCH",event)
+    event.respondWith(
+      caches.match(event.request, {ignoreSearch:true}).then(response => {
+          console.log("RESPONDING WITH",response,"OR",event.request)
+        return response || fetch(event.request);
+      })
+    );
+  });
